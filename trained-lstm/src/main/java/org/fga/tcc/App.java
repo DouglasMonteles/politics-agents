560fef32a260d9c3282fe7cc2a3017e53122ddf6
DouglasMonteles <silvadouglas328@gmail.com>
2024-11-18 20:52:25 -0300

    :sparkles: add voting model facade

--- trained-lstm/src/main/java/org/fga/tcc/App.java
+++ trained-lstm/src/main/java/org/fga/tcc/App.java
@@ -1,33 +1,14 @@
 package org.fga.tcc;
 
-import org.fga.tcc.services.VotingModelService;
-import org.fga.tcc.utils.FileUtils;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.fga.tcc.facades.VotingModelFacade;
 
 public class App {
 
     public static void main( String[] args ) {
-        VotingModelService votingModelService = VotingModelService.getInstance();
-
+        VotingModelFacade votingModelFacade = VotingModelFacade.getInstance();
         String parentDirectory = System.getProperty("user.dir") + "/trained-data/votes/partyOrientation/proposalDescription";
 
-        // TODO: considerar somente os que tiverem 0.txt e 1.txt
-        List<String> excludedDirectories = new ArrayList<>();
-        excludedDirectories.add("/home/douglas/Documentos/www/politics-agents/trained-data/votes/partyOrientation/proposalKeywords/PRD");
-        excludedDirectories.add("/home/douglas/Documentos/www/politics-agents/trained-data/votes/partyOrientation/proposalDescription/PRD");
-
-        FileUtils.iterateDirectory(parentDirectory, (partyAcronymChildDirectory) -> {
-            String path = partyAcronymChildDirectory.toAbsolutePath().toString();
-
-            if (!excludedDirectories.contains(path)) {
-                votingModelService
-                    .setModelPath(path)
-                    .prepareWordVector()
-                    .trainModel();
-            }
-        });
+        votingModelFacade.generateTrainedModel(parentDirectory);
     }
 
 }
---
+++ trained-lstm/src/main/java/org/fga/tcc/facades/VotingModelFacade.java
@@ -0,0 +1,41 @@
+package org.fga.tcc.facades;
+
+import org.fga.tcc.services.VotingModelService;
+import org.fga.tcc.utils.FileUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VotingModelFacade {
+
+    private static final VotingModelFacade INSTANCE = new VotingModelFacade();
+
+    private final VotingModelService votingModelService;
+
+    public VotingModelFacade() {
+        this.votingModelService = VotingModelService.getInstance();
+    }
+
+    public void generateTrainedModel(String parentDirectory) {
+        // TODO: considerar somente os que tiverem 0.txt e 1.txt
+        List<String> excludedDirectories = new ArrayList<>();
+        excludedDirectories.add("/home/douglas/Documentos/www/politics-agents/trained-data/votes/partyOrientation/proposalKeywords/PRD");
+        excludedDirectories.add("/home/douglas/Documentos/www/politics-agents/trained-data/votes/partyOrientation/proposalDescription/PRD");
+
+        FileUtils.iterateDirectory(parentDirectory, (partyAcronymChildDirectory) -> {
+            String path = partyAcronymChildDirectory.toAbsolutePath().toString();
+
+            if (!excludedDirectories.contains(path)) {
+                this.votingModelService
+                    .setModelPath(path)
+                    .prepareWordVector()
+                    .trainModel();
+            }
+        });
+    }
+
+    public static VotingModelFacade getInstance() {
+        return INSTANCE;
+    }
+
+}
